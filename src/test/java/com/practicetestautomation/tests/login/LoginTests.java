8bd5bc22adf5520b9cd421b3ef2069bdc79e52ab
Arthur Bozelli <95321549+Bozelli9@users.noreply.github.com>
2025-02-17 16:02:13 -0300

    Adicionado switch-case para configurar WebDriver para dois navegadores diferentes (Chrome e Firefox). Atualizado o arquivo TestNG XML para executar testes positivos e negativos em ambos os navegadores.

--- src/test/java/com/practicetestautomation/tests/login/LoginTests.java
+++ src/test/java/com/practicetestautomation/tests/login/LoginTests.java
@@ -4,59 +4,99 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
 public class LoginTests {
-
     private WebDriver driver;
 
-
     @BeforeMethod(alwaysRun = true)
-    public void beforeSetUp(){
-        driver = new ChromeDriver();
+    @Parameters("browser")
+    public void setUp(String browser) {
+        System.out.println("Running test in " + browser);
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            default:
+                driver = new ChromeDriver();
+                break;
+        }
+
         driver.get("https://practicetestautomation.com/practice-test-login/");
     }
 
-
     @AfterMethod(alwaysRun = true)
-    public void setUp() {
+    public void tearDown() {
         driver.quit();
-
     }
 
     @Test(groups = {"positive", "regression", "smoke"})
-    public void testLoginFuncionality() {
+    public void testLoginFunctionality() {
 
-        WebElement usernameText = driver.findElement(By.id("username"));
-        usernameText.sendKeys("student");
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        usernameInput.sendKeys("student");
 
-        WebElement passwordText = driver.findElement(By.id("password"));
-        passwordText.sendKeys("Password123");
 
-        WebElement submitBtn = driver.findElement(By.id("submit"));
-        submitBtn.click();
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        passwordInput.sendKeys("Password123");
 
-        String anotherUrl = "https://practicetestautomation.com/logged-in-successfully/";
-        String currentUrl = driver.getCurrentUrl();
-        Assert.assertEquals(currentUrl, anotherUrl);
 
-        String loggedSucess = "Congratulations student. You successfully logged in!";
+        WebElement submitButton = driver.findElement(By.id("submit"));
+        submitButton.click();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        String expectedUrl = "https://practicetestautomation.com/logged-in-successfully/";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl, expectedUrl);
+
+        String expectedMessage = "Congratulations student. You successfully logged in!";
         String pageSource = driver.getPageSource();
-        Assert.assertTrue(pageSource.contains(loggedSucess));
+        Assert.assertTrue(pageSource.contains(expectedMessage));
 
 
-        WebElement buttonSubmit = driver.findElement(By.linkText("Log out"));
-        Assert.assertTrue(buttonSubmit.isDisplayed());
+        WebElement logOutButton = driver.findElement(By.linkText("Log out"));
+        Assert.assertTrue(logOutButton.isDisplayed());
+    }
+
+    @Parameters({"username", "password", "expectedErrorMessage"})
+    @Test(groups = {"negative", "regression"})
+    public void negativeLoginTest(String username, String password, String expectedErrorMessage) {
+
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        usernameInput.sendKeys(username);
 
 
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        passwordInput.sendKeys(password);
+
+
+        WebElement submitButton = driver.findElement(By.id("submit"));
+        submitButton.click();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        WebElement errorMessage = driver.findElement(By.id("error"));
+        Assert.assertTrue(errorMessage.isDisplayed());
+
+        String actualErrorMessage = errorMessage.getText();
+        Assert.assertEquals(actualErrorMessage, expectedErrorMessage);
     }
 }
